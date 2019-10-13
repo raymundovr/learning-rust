4c77f5f7710e36b611a91293716ca6f4d0bfc847
Raymundo Vasquez Ruiz <raymundo.vr@protonmail.com>
2019-10-13 18:45:04 +0200

    mean is now obtained as f32

--- excersises/ch08/one/src/main.rs
+++ excersises/ch08/one/src/main.rs
@@ -7,15 +7,32 @@ fn main() {
     // median (when sorted, the value in the middle position), and mode
     // (the value that occurs most often; a hash map will be helpful here) of the list.
     let random_ints = generate_random_integers(10);
-    println!("{:?}", random_ints);
+    println!("Set of integers {:?}", random_ints);
+
+    println!("Mean: {}", calculate_mean(&random_ints));
 }
 
 
 fn generate_random_integers(quantity: u8) -> Vec<u8> {
     let mut random_ints: Vec<u8> = Vec::new();
-    for x in 0..quantity {
+    let mut x = 0;
+    while x < quantity {
 	random_ints.push(rand::thread_rng().gen_range(0, 11));
+	x += 1;
     }
     
     random_ints
+}
+
+
+fn calculate_mean(integers: &Vec<u8>) -> f32 {
+    let mut sum = 0.0;
+
+    for i in integers {
+	sum += f32::from(*i);
+    }
+
+    println!("--Total sum: {}", sum);
+    
+    sum / (integers.len() as f32)
 }
