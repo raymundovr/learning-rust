c3d788ad2d4e1b3b0943ade5cf2df5ec93ed338c
Raymundo Vasquez Ruiz <raymundo.vr@protonmail.com>
2019-10-13 11:08:55 +0200

    started excersises

---
+++ excersises/ch08/one/src/main.rs
@@ -0,0 +1,15 @@
+extern crate rand;
+
+use rand::
+
+fn main() {
+    // Given a list of integers, use a vector and return the mean (the average value),
+    // median (when sorted, the value in the middle position), and mode
+    // (the value that occurs most often; a hash map will be helpful here) of the list.
+    println!("Hello, world!");
+}
+
+
+fn generate_random_integers(quantity) -> Vec<i32> {
+    
+}
