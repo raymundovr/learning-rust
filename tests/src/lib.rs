8b7dfcfc31e0d0fb11a6ee8ea46150e393e117ef
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2019-10-30 21:26:31 +0100

    tests chapter

---
+++ tests/src/lib.rs
@@ -0,0 +1,55 @@
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+
+//     #[test]
+//     fn it_fails() {
+//         panic!("Make this test fail!");
+//     }
+// }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 10,
+            height: 10,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 10,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+}
